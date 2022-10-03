7c5b561f772755669533cebbad0f455563bc3539
Yuriy <urijlamihin@gmail.com>
2022-10-03 22:51:00 +1000

    commit 3

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	# Инициализирует pygame, settings и объект экрана. 
@@ -14,13 +15,6 @@ def run_game():
 	
 	# Запуск основного цикла игры
 	while True:
-		# Отслежиание событий клавиатуры и мыши
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-		#При каждом проходе цикла перерисовыается экран.
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
-		# Отображение последнего прорисованного экрана.
-		pygame.display.flip()
+		gf.check_events()
+		gf.update_screen(ai_settings, screen, ship)
 run_game()
