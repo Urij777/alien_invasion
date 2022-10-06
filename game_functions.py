1f6f0cfcafeb68faad4227e0f3125c9c1218ccce
Yuriy <urijlamihin@gmail.com>
2022-10-06 15:41:15 +1000

    Итог главы 12

--- game_functions.py
+++ game_functions.py
@@ -3,16 +3,20 @@ import pygame
 from bullet import Bullet
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
-	"""."""
+	"""Реагирует на нажатия клавиш."""
 	if event.key == pygame.K_RIGHT:
 		ship.moving_right = True
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = True
 	elif event.key == pygame.K_SPACE:
-		# Создание новой пули и включение её в группу bullets.
-		if len(bullets) < ai_settings.bullets_allowed:
-			new_bullet = Bullet(ai_settings, screen, ship)
-			bullets.add(new_bullet)
+		fire_bullet(ai_settings, screen, ship, bullets)
+		
+def fire_bullet(ai_settings, screen, ship, bullets):
+	"""Выпускает пулю, если максимум ещё не достигнут."""
+	# Создание новой пули и включение её в группу bullets.
+	if len(bullets) < ai_settings.bullets_allowed:
+		new_bullet = Bullet(ai_settings, screen, ship)
+		bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
 	"""."""
@@ -22,7 +26,7 @@ def check_keyup_events(event, ship):
 		ship.moving_left = False
 
 def check_events(ai_settings, screen, ship, bullets):
-	"""Обрабатывает нажатия клаиш и события мыши."""
+	"""Обрабатывает нажатия клавиш и события мыши."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
@@ -41,3 +45,12 @@ def update_screen(ai_settings, screen, ship, bullets):
 	ship.blitme()
 	# Отображение последнего прорисованного экрана.
 	pygame.display.flip()
+
+def update_bullets(bullets):
+	"""Обновляет позиции пуль и уничтожает старые пули."""
+	# Обновление позиций пуль.
+	bullets.update()
+	# Удаление пуль, вышедших за край экрана.
+	for bullet in bullets.copy():
+		if bullet.rect.bottom <= 0:
+			bullets.remove(bullet)
