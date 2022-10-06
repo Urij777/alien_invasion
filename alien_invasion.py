3843742e60a4b7edfc0415ac67a7e1b1c9d1d1e1
Yuriy <urijlamihin@gmail.com>
2022-10-06 15:04:38 +1000

    Ограничение количества пуль (<3)

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,6 +20,10 @@ def run_game():
 		gf.check_events(ai_settings,screen, ship, bullets)
 		ship.update()
 		bullets.update()
+		# Удаление пуль, вышедших за край экрана.
+		for bullet in bullets.copy():
+			if bullet.rect.bottom <= 0:
+				bullets.remove(bullet)
 		gf.update_screen(ai_settings, screen, ship, bullets)
 		
 run_game()
