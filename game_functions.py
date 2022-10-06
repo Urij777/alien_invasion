f19caac0a69391ed8e0e1f5cafa3f4d1b6e93e2d
Yuriy <urijlamihin@gmail.com>
2022-10-06 13:48:11 +1000

    Перемещение влево и вправо

--- game_functions.py
+++ game_functions.py
@@ -9,11 +9,15 @@ def check_events(ship):
 		elif event.type == pygame.KEYDOWN:
 			# Переместить корабль вправо.
 			if event.key == pygame.K_RIGHT:
-				ship.rect.centerx += 1
 				ship.moving_right = True
+			elif event.key == pygame.K_LEFT:
+				ship.moving_left = True
 		elif event.type == pygame.KEYUP:
 			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False		
+				ship.moving_right = False	
+			elif event.key == pygame.K_LEFT:
+				ship.moving_left = False	
+
 def update_screen(ai_settings, screen, ship):
 	"""Обновляет изображения на экране и отображает новый экран."""
 	#При каждом проходе цикла перерисовыается экран.
