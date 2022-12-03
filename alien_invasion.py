1958bd31e57bab7e8390eb6a65a5d307275cf317
Yuriy <urijlamihin@gmail.com>
2022-12-04 04:33:42 +1000

    End version.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 import game_functions as gf
@@ -22,16 +23,18 @@ def run_game():
 	# Создание флота пришельцев.
 	gf.create_fleet(ai_settings,screen, ship, aliens)
 
-	# Создание экземпляра для хранения игровой статистики.
+	# Создание экземпляров GameStats и Scoreboard.
 	stats = GameStats(ai_settings)
+	sb = Scoreboard(ai_settings, screen, stats)
 
 	# Запуск основного цикла игры
 	while True:
-		gf.check_events(ai_settings,screen, ship, bullets)
+		gf.check_events(ai_settings,screen, stats, sb, play_button, ship, aliens, bullets)
 		if stats.game_active:
 			ship.update()
-			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-			gf.update_aliens(ai_settings, stats, screen,  ship, aliens, bullets)
-		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+			gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+		
 		
 run_game()
